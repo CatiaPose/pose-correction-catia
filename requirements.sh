6a292ec8c383fa1dfb00706fc92f5250cdc610c0
CatiaPose <159644471+CatiaPose@users.noreply.github.com>
2024-02-11 00:05:41 +0500

    Update and rename requirements.txt to requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,8 +1,9 @@
 pip install fastapi
-pip install unicorn
+pip install uvicorn
 pip install opencv-python
 pip install mediapipe
 pip install scipy
 pip install imageio
 pip install python-multipart
 pip install imageio[pyav]
+
